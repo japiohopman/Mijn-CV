4825a51e50cd7106d944069dba3a794c88e81e67
Jaaphopman <japihopman@gmai.com>
2022-01-06 15:02:20 +0100

    version.2

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // jshint esversion:6
+function myFunction() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
 
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+} 
+
+// maby dont need
 const express = require("express");
 const bodyParser = require("body-parser");
 
